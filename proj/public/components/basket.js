f096e6cd5abfb0c426c94681dbfbfe2238525eb8
KrinickiJ.V.A <krinickij.v.a@gmail.com>
2022-03-10 19:12:40 +0300

    mu worck

---
+++ proj/public/components/basket.js
@@ -0,0 +1,61 @@
+Vue.component('basket',{
+    props:{
+        'basketVisibility':Boolean,
+    },
+    data(){
+        return{
+            cartUrl:'/api/cart',
+            cartItems:[],
+        }
+    },
+    methods:{
+        getJson(url){
+            return fetch(url)
+                .then(result => result.json())
+                .catch(error =>{
+                    console.log(error)
+                })
+        },
+        deleteItem(product){
+            console.log(product.id)
+            this.$root.deleteJson(`api/cart/${product.id}`,{count:1});
+        }
+    },
+    computed:{},
+    mounted(){},
+    created(){
+        this.getJson(this.cartUrl)
+            .then(data =>{
+                for(let el of data){
+                    this.cartItems.push(el)
+                }
+            })
+    },
+    watch:{
+        cartItems:[]
+    },
+    template:`
+        <div class="header__basket" :class="{hiddenBlock : basketVisibility}">
+            <div class="header__basker-container">
+                <p v-if="cartItems.length < 1">Корзина пуста</p>
+                <div 
+                    class="catr-item" 
+                    v-for="item of cartItems" 
+                    :keys="item.id"
+                    >
+                    <div class="card-item-info">
+                        <p class="product__title">{{item.name}}</p>
+                        <p class="product__price">{{item.price}}</p>
+                        <p>количество {{item.count}}</p>
+                    </div>
+                    <button
+                     class="card-item-del"
+                      :data-id="item.id"
+                        @click="deleteItem(item)"
+                      >del</button>
+                </div>
+            </div>
+        </div>
+    `
+});
+
